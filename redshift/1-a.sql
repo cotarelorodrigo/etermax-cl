4b7817e8ad0f98367fa5a8033ecce8e12b3768db
Rodrigo Cotarelo <cotarelorodrigo@gmail.com>
2021-02-14 18:37:03 -0300

    Ejercicio 1-a

---
+++ redshift/1-a.sql
@@ -0,0 +1,15 @@
+select 
+dm.fecha, 
+dc.descripcion as descripcion_de_cliente,
+prov.descripcion as descripcion_de_proveedor,
+dp.descripcion as descripcion_de_producto,
+marcas.descripcion as descripcion_de_marca,
+dm.cantidad,
+dm.costo,
+dm.venta,
+dm.venta - dm.costo as ganancia_neta
+from data_movimientos as dm
+join data_clientes as dc on dm.cod_cliente = dc.cod_cliente
+join data_productos as dp on dm.cod_prod = dp.cod_prod
+join data_marcas as marcas on dp.cod_marca = marcas.cod_marca
+join data_proveedores as prov on dp.cod_proveedor = prov.cod_proveedor;
